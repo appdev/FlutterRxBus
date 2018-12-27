5b5423686adb4e83659f4ecc6e5b9e331d8664db
zhaoshulin <zhaoshulin@gouuse.cn>
2018-12-27 19:33:57 +0800

    first commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rxbus/rxbus.dart';
-import 'package:rxbus_example/new_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -83,6 +82,41 @@ class _MyAppState extends State<MyApp> {
               Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: Text('First Page'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NewPage extends StatefulWidget {
+  @override
+  _NewPageState createState() => _NewPageState();
+}
+
+class _NewPageState extends State<NewPage> {
+  String received;
+  String title = "EventBus example app";
+  final String TAG = "EventBus";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: RaisedButton(
+                    child: Text('Update First Page Title'),
+                    onPressed: () =>
+                        RxBus.post(ChangeTitleEvent("Update Title by other page"), tag: TAG)),
               ),
             ],
           ),
